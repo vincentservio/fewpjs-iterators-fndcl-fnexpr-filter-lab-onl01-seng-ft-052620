2ed90e5b891ead55c28833e8b576cf64fb47c13c
Vincent Servio <vincentservio@gmail.com>
2020-09-05 16:27:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+ function   findMatching(drivers, string) {
+   let driver = drivers.filter(n =>  n === string || n == string.toLowerCase())
+      return driver
+ 
+ }
+ 
+ function   fuzzyMatch(drivers, string) {
+ let sl = string.length 
+    
+.filter(n => n))
+   return driver
+ }
